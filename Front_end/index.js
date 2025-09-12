29d6ba0647998d4a53ff2d8180514ac37adce34f
camp_.ksk <camp131249@gmail.com>
2025-09-12 15:50:22 +0700

    Change default port frontend

--- Front_end/index.js
+++ Front_end/index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"))
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3222;
 app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
